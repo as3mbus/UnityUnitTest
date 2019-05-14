513f7772beb2a6182cf67dfc215721e1e75d99a7
A. Billy Firman Prasetya S <billy.firman@agate.id>
2019-05-14 13:35:01 +0700

    Create Failing Test for queued Movement 2

--- Assets/Scripts/PlayTest/MovementTest.cs
+++ Assets/Scripts/PlayTest/MovementTest.cs
@@ -8,27 +8,18 @@ namespace Tests
 {
     public class MovementTest
     {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void NewTestScriptSimplePasses()
+        GameObject testObj;
+        Mover testMov;
+        
+        [SetUp]
+        public void setup()
         {
-            // Use the Assert class to test conditions
-        }
-
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator NewTestScriptWithEnumeratorPasses()
-        {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
+            testObj = new GameObject();
+            testMov = testObj.AddComponent<Mover>();
         }
         [UnityTest]
         public IEnumerator translateObject()
         {
-            GameObject testObj = new GameObject();
-            Mover testMov = testObj.AddComponent<Mover>();
             Vector2 pos = new Vector2(3, 5);
             testMov.TranslateObject(pos);
             yield return new WaitUntil(() => !testMov.IsMoving);
@@ -36,9 +27,7 @@ namespace Tests
         }
         [UnityTest]
         public IEnumerator stopMovement()
-        {
-            GameObject testObj = new GameObject();
-            Mover testMov = testObj.AddComponent<Mover>();
+        {   
             Vector2 pos = new Vector2(99, 99);
             testMov.TranslateObject(pos);
             yield return new WaitForFixedUpdate();
@@ -48,6 +37,23 @@ namespace Tests
             yield return null;
             Assert.That(afterStopPos, Is.Not.EqualTo(Vector2.zero));
             Assert.That(afterStopPos, Is.EqualTo((Vector2)testObj.transform.position));
+        }
+        [UnityTest]
+        public IEnumerator MovementQueue()
+        {
+            Vector2 firstMove = new Vector2(5,5);
+            Vector2 firstMoveGoal = (Vector2) testObj.transform.position + firstMove;
+            testMov.TranslateObject(firstMove);
+            Vector2 seconMove = new Vector2(-9,-10);
+            Vector2 seconMoveGoal = firstMoveGoal + seconMove;
+            testMov.addMove(seconMove);
+            yield return new WaitForFixedUpdate();
+            yield return new WaitUntil(()=>testMov.PendingMove == 0);
+            Assert.That(Vector2.Distance(testObj.transform.position, firstMoveGoal), Is.LessThanOrEqualTo(0.1));
+            yield return new WaitUntil(()=> !testMov.IsMoving);
+            Assert.That(Vector2.Distance(testObj.transform.position, seconMoveGoal), Is.LessThanOrEqualTo(0.1));
+
+
         }
     }
 }
