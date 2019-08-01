9bc9e60f78661bd8ac568cb043149423a400a3d0
A. Billy Firman Prasetya S <billy.firman@agate.id>
2019-08-01 16:28:26 +0700

    Update Readme and scripts for batch build
    add code coverage experimentation section

---
+++ Scripts/unity_headless_test.bat
@@ -0,0 +1,11 @@
+start /WAIT^
+ D:\Software-Req\Unity3D\2018.3.11f1\Editor\Unity.exe^
+ -runTests^
+ -ProjectPath D:\Projects\Unity\onboard-crossword-Copy\^
+ -testResults D:\UnityTestResult\testResult-0.0.1.2.xml^
+ -testPlatform editmode^
+ -logFile D:\UnityTestResult\Logfiles-0.0.1.2.txt^
+ -batchMode^
+ -nographics^
+ -noUpm
+
